978967b4ce8afc5c3427cadb9d50b2dfcb265d7c
Christian Studer <cstuder@existenz.ch>
2026-04-26 21:34:10 +0200

    Initial commit of @wordpress/create-block@latest

---
+++ build/infomail-block/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '0b0bf1afc44c18779d0c');
---
+++ build/infomail-block/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'fa8b5c8ed6e2c1e03326');
